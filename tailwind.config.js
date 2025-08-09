d4a9d7f3f9be7929db22e263f956971d4a66ae4f
LastSother <lastsother1@gmail.com>
2025-08-09 13:34:57 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'city-blue': '#4a90e2',
+        'city-green': '#50c878',
+        'city-bg': '#eef2f7',
+      },
+    },
   },
   plugins: [],
 }
